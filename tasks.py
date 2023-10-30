60b15bc62dc1096991af262bf9e99b2e0f279125
Justin Mayer <entroP@gmail.com>
2023-10-30 08:42:23 +0100

    Switch Invoke tasks from Poetry to PDM

--- tasks.py
+++ tasks.py
@@ -1,14 +1,15 @@
 from inspect import cleandoc
+import logging
 import os
 from pathlib import Path
 from shutil import which
-import sys
 
 from invoke import task
 
+logger = logging.getLogger(__name__)
+
 PKG_NAME = "photos"
 PKG_PATH = Path(f"pelican/plugins/{PKG_NAME}")
-TOOLS = ("poetry", "pre-commit")
 
 ACTIVE_VENV = os.environ.get("VIRTUAL_ENV", None)
 VENV_HOME = Path(os.environ.get("WORKON_HOME", "~/.local/share/virtualenvs"))
@@ -16,16 +17,19 @@ VENV_PATH = Path(ACTIVE_VENV) if ACTIVE_VENV else (VENV_HOME.expanduser() / PKG_
 VENV = str(VENV_PATH.expanduser())
 BIN_DIR = "bin" if os.name != "nt" else "Scripts"
 VENV_BIN = Path(VENV) / Path(BIN_DIR)
-POETRY = which("poetry") if which("poetry") else (VENV_BIN / "poetry")
-CMD_PREFIX = f"{VENV_BIN}/" if ACTIVE_VENV else f"{POETRY} run "
+
+TOOLS = ("pdm", "pre-commit")
+PDM = which("pdm") if which("pdm") else (VENV_BIN / "pdm")
+CMD_PREFIX = f"{VENV_BIN}/" if ACTIVE_VENV else f"{PDM} run "
 PRECOMMIT = which("pre-commit") if which("pre-commit") else f"{CMD_PREFIX}pre-commit"
 PTY = os.name != "nt"
 
 
 @task
-def tests(c):
-    """Run the test suite."""
-    c.run(f"{CMD_PREFIX}pytest", pty=PTY)
+def tests(c, deprecations=False):
+    """Run the test suite, optionally with `--deprecations`."""
+    deprecations_flag = "" if deprecations else "-W ignore::DeprecationWarning"
+    c.run(f"{CMD_PREFIX}pytest {deprecations_flag}", pty=PTY)
 
 
 @task
@@ -64,34 +68,34 @@ def tools(c):
     """Install development tools in the virtual environment if not already on PATH."""
     for tool in TOOLS:
         if not which(tool):
+            logger.info(f"** Installing {tool} **")
             c.run(f"{CMD_PREFIX}pip install {tool}")
 
 
 @task
 def precommit(c):
     """Install pre-commit hooks to .git/hooks/pre-commit."""
+    logger.info("** Installing pre-commit hooks **")
     c.run(f"{PRECOMMIT} install")
 
 
 @task
 def setup(c):
     """Set up the development environment."""
-    if which("poetry") or ACTIVE_VENV:
+    if which("pdm") or ACTIVE_VENV:
         tools(c)
-        c.run(f"{CMD_PREFIX}python -m pip install --upgrade pip")
-        c.run(f"{POETRY} install")
+        c.run(f"{CMD_PREFIX}python -m pip install --upgrade pip", pty=PTY)
+        c.run(f"{PDM} update --dev", pty=PTY)
         precommit(c)
-        print(  # noqa: T201
-            "\nDevelopment environment should now be set up and ready!\n"
-        )
+        logger.info("\nDevelopment environment should now be set up and ready!\n")
     else:
         error_message = """
-            Poetry is not installed, and there is no active virtual environment available.
+            PDM is not installed, and there is no active virtual environment available.
             You can either manually create and activate a virtual environment, or you can
-            install Poetry via:
+            install PDM via:
 
-            curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+            curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py | python3 -
 
             Once you have taken one of the above two steps, run `invoke setup` again.
             """  # noqa: E501
-        sys.exit(cleandoc(error_message))
+        raise SystemExit(cleandoc(error_message))
